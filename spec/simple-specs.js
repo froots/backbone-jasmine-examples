9648efe29372ed0e530759396a85a98032a70461
James Newbery <james@tinnedfruit.com>
2011-03-03 07:52:59 +0000

    Added simple anonymous spy spec

--- spec/simple-specs.js
+++ spec/simple-specs.js
@@ -13,6 +13,16 @@ describe("simple specs", function() {
         .toEqual("Hollywood - Part 2");
     });
     
+    it("should fire a callback when 'foo' is triggered", function() {
+      var spy = sinon.spy();
+      
+      this.episode.bind('foo', spy);
+      
+      this.episode.trigger('foo');
+      
+      expect(spy).toHaveBeenCalled();
+    });
+    
   });
   
 
