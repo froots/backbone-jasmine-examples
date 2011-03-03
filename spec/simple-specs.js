4d59d35d9f874a4fbb3205e0dfa063e75bdc55ae
James Newbery <james@tinnedfruit.com>
2011-03-03 08:36:13 +0000

    Fake server example

--- spec/simple-specs.js
+++ spec/simple-specs.js
@@ -4,7 +4,8 @@ describe("simple specs", function() {
     
     beforeEach(function() {
       this.episode = new Episode({
-        title: "Hollywood - Part 2"
+        title: "Hollywood - Part 2",
+        id: 123
       });
     })
     
@@ -32,6 +33,50 @@ describe("simple specs", function() {
       expect(spy.getCall(0).args[0].url).toEqual("#");
       
       jQuery.ajax.restore();
+    });
+  
+    describe("when saving", function() {
+      
+      beforeEach(function() {
+        this.server = sinon.fakeServer.create();
+      });
+
+      afterEach(function() {
+        this.server.restore();
+      });
+      
+      it("should fire the change event", function() {
+        var callback = sinon.spy();
+        
+        var episode = new Episode({
+          url: "/episode/123"
+        });
+
+        // Set how the fake server will respond
+        // This reads: a GET request for /episode/123 will return a 
+        // 200 response of type application/json with the given JSON response body
+        this.server.respondWith('{"id":123,"title":"Hollywood - Part 2"}');
+
+        // Bind to the change event on the model
+        episode.bind('change', callback);
+
+        // makes an ajax request to the server
+        episode.fetch(); 
+
+        // Fake server responds to the request
+        this.server.respond(); 
+
+        // Expect that the spy was called with the newly updated model
+        expect(callback).toHaveBeenCalled();
+        expect(callback.getCall(0).args[0].attributes)
+          .toEqual({
+            id: 123,
+            title: "Hollywood - Part 2",
+            url: "/episode/123"
+          });
+        
+      });
+      
     });
     
   });
